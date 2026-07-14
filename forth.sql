30a45004e3d5437190d8568f04a3990c1ca9050a
Rishav kumar <65082283+rish3119@users.noreply.github.com>
2026-07-14 23:03:17 +0530

    Add files via upload

---
+++ forth.sql
@@ -0,0 +1,14 @@
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+    Query all columns for a city in CITY with the ID 1661.
+
+    The CITY table is described as follows:
+*/
+
+SELECT *
+FROM CITY
+WHERE ID = 1661;
