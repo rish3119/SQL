a6fed7cc790fe1653c6ccb59a26f8e0934b9cbc7
Rishav kumar <65082283+rish3119@users.noreply.github.com>
2026-07-19 23:18:12 +0530

    Add files via upload

---
+++ seventh.sql
@@ -0,0 +1,13 @@
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+    Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+    The STATION table is described as follows:
+*/
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE MOD(ID, 2) = 0;
